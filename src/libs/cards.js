77243da0f84398dc7f2a9e286b025871c1cf265b
Federico Iglesias <igl.fede@gmail.com>
2025-07-28 16:52:56 +0200

    fix_cards

--- src/libs/cards.js
+++ src/libs/cards.js
@@ -4,7 +4,8 @@ const footer = document.getElementsByTagName("footer");
 const condition = window.scrollY + window.innerHeight >= html[0].scrollHeight;
 document.addEventListener("scroll", function () {
   console.log(window.scrollY + window.innerHeight >= html[0].scrollHeight);
-  if (window.scrollY + window.innerHeight  >= html[0].scrollHeight)
+  console.log(window.scrollY, window.innerHeight, html[0].scrollHeight);
+  if (window.scrollY + window.innerHeight + 50  >= html[0].scrollHeight)
     card.setAttribute("style", "display: flex;");
   else card.setAttribute("style", "display: none;");
 });
