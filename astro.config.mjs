622fa2b056105bc122fb4b0196f8c09b53e43dad
Federico Iglesias <igl.fede@gmail.com>
2025-07-30 23:32:15 +0200

    fix_paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://FedericoIglesias.github.io',
-  base: 'movementStudio',
+  base: 'movementStudio/',
 });
